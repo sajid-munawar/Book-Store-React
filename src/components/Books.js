ea485c265a049f7a0816c9e1640f6513b27f355e
sajid-munawar <sajidmunawar666@gmail.com>
2022-07-16 20:47:52 +0500

    Use map method to show books

--- src/components/Books.js
+++ src/components/Books.js
@@ -4,11 +4,22 @@ import Form from './form';
 import Navbar from './Navbar';
 
 function Books() {
-  const bookList = useState([{ title: '', author: '' }]);
+  const bookList = useState([
+    { title: 'Design Patterns', author: 'Erich Gamma' },
+    { title: 'Patterns of Enterprise Application Architecture', author: ' Martin Fowler' },
+    { title: 'Code Complete', author: 'Steve Mcconnell' },
+    { title: 'Enterprise Integration Patterns', author: 'Gregor Hohpe' },
+  ]);
   return (
     <div>
       <Navbar />
-      {bookList.forEach((book) => (<Book title={book.title} author={book.author} />))}
+      {bookList[0].map((book) => (
+        <Book
+          key={book.title}
+          title={book.title}
+          author={book.author}
+        />
+      ))}
       <Form />
     </div>
   );
