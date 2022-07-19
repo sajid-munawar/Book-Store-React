9c5b7a3393925a6bbd4e9f104496d0e86e4e4c2d
sajid-munawar <sajidmunawar666@gmail.com>
2022-07-19 23:56:56 +0500

    Update directory structure

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import {
   Routes,
   Route,
 } from 'react-router-dom';
-import App from './App';
+import Books from './components/Books';
 import Categories from './components/Categories';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<Books />} />
       <Route path="categories" element={<Categories />} />
     </Routes>
   </BrowserRouter>,
