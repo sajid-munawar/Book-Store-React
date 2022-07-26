dd0a1950f38bc0fc0cff15c1916aeb08712706f0
sajid-munawar <sajidmunawar666@gmail.com>
2022-07-26 22:03:24 +0500

    Implement Get books and add book

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
-import { removeBook } from '../redux/books/books';
+import { deleteBook } from '../redux/books/apiReducer';
 
 function Book({ id, title, author }) {
   const dispatch = useDispatch();
@@ -17,7 +17,7 @@ function Book({ id, title, author }) {
         {author}
         {' '}
       </h4>
-      <button type="button" onClick={() => dispatch(removeBook(id))}>Remove</button>
+      <button type="button" onClick={() => dispatch(deleteBook(id))}>Remove</button>
     </div>
   );
 }
--- src/components/Books.js
+++ src/components/Books.js
@@ -1,16 +1,23 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getAllBooks } from '../redux/books/apiReducer';
 import Book from './Book';
 import Navbar from './Navbar';
 import Form from './form';
 
 function Books() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllBooks());
+  }, []);
+
   const books = useSelector((state) => state.book);
   const booksUi = books.map((book) => (
     <Book
-      key={book.title}
+      key={book.item_id}
       title={book.title}
       author={book.author}
-      id={book.id}
+      id={book.item_id}
     />
   ));
   return (
